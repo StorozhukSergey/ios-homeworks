73b10490f9e5dcf8e273d11442816eebff8d3404
StorozhukSergey <118775462+StorozhukSergey@users.noreply.github.com>
2023-01-02 14:20:45 +0300

    Ver. 1.1

--- Netology_IB_Instruments/ProfileViewController.swift
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -11,9 +11,16 @@ class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+        loadNib()
     }
 
+    func loadNib() {
+        if let nibView = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? ProfileView {
+                nibView.frame = CGRect(x: 0, y: 150, width: UIScreen.main.bounds.width, height: 400)
+                view.addSubview(nibView)
+        }
+    }
 
 }
 
